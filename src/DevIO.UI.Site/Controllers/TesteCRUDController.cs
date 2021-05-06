fcf6fd36f30c509842ab96e9660f9366b2c80da4
Eduardo Felipe <eduardo_donadio@outlook.com>
2021-05-06 20:11:25 -0300

    Instalando o Entity Framework e criando a migration inicial

---
+++ src/DevIO.UI.Site/Controllers/TesteCRUDController.cs
@@ -0,0 +1,46 @@
+﻿using DevIO.UI.Site.Data;
+using DevIO.UI.Site.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.UI.Site.Controllers
+{
+    public class TesteCRUDController : Controller
+    {
+        private readonly MeuDbContext _contexto;
+
+        public TesteCRUDController(MeuDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno
+            {
+                Nome = "Eduardo",
+                DataNascimento = DateTime.Now,
+                Email = "email@email.com"
+            };
+
+            _contexto.Alunos.Add(aluno); //Adicionando o aluno na memória do entity framework
+            _contexto.SaveChanges(); //Salvando no banco
+
+            var aluno2 = _contexto.Alunos.Find(aluno.Id); //Buscando por ID
+            var aluno3 = _contexto.Alunos.FirstOrDefault(a => a.Email == "email@email.com"); //Buscando por Email
+            var aluno4 = _contexto.Alunos.Where(a => a.Nome == "Eduardo"); //Buscando uma coleção de alunos
+
+            aluno.Nome = "João";
+            _contexto.Alunos.Update(aluno); //Fazendo um Update na memória
+            _contexto.SaveChanges(); //Salvando as alterações no banco
+
+            _contexto.Alunos.Remove(aluno);
+            _contexto.SaveChanges();
+
+            return View("_Layout");
+        }
+    }
+}
--- src/DevIO.UI.Site/Startup.cs
+++ src/DevIO.UI.Site/Startup.cs
@@ -1,12 +1,10 @@
+using DevIO.UI.Site.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevIO.UI.Site
 {
@@ -22,7 +20,11 @@ namespace DevIO.UI.Site
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MeuDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MeuDbContext")));
+
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
+
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
